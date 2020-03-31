3273c173dd92e3f208573827d6290804c0968dfe
ronanhub <45386974+ronanhub@users.noreply.github.com>
2020-03-31 12:26:55 +0200

    test aanpassing

--- Scroll test/Form1.Designer.cs
+++ Scroll test/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
+        /// bladieblalalalallal
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
