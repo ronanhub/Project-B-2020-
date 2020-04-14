99d82401647d81d1d9651e1d568566919cf7471d
keremz <keremz@users.noreply.github.com>
2020-04-14 11:09:43 +0200

    Homepage

---
+++ EersteProjectMau/EersteProjectMau/HomePage.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace EersteProjectMau
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.navigatiePanel = new System.Windows.Forms.Panel();
+            this.openHelp = new System.Windows.Forms.Button();
+            this.faqButton = new System.Windows.Forms.Button();
+            this.contactButton = new System.Windows.Forms.Button();
+            this.agendaButton = new System.Windows.Forms.Button();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.searchBar = new System.Windows.Forms.TextBox();
+            this.helpPanel1 = new System.Windows.Forms.Panel();
+            this.uitlegLabel1 = new System.Windows.Forms.Label();
+            this.huidigePaginaLabel1 = new System.Windows.Forms.Label();
+            this.hulpLabel1 = new System.Windows.Forms.Label();
+            this.logoBox = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.navigatiePanel.SuspendLayout();
+            this.helpPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navigatiePanel
+            // 
+            this.navigatiePanel.Controls.Add(this.faqButton);
+            this.navigatiePanel.Controls.Add(this.contactButton);
+            this.navigatiePanel.Controls.Add(this.agendaButton);
+            this.navigatiePanel.Controls.Add(this.homeButton);
+            this.navigatiePanel.Controls.Add(this.openHelp);
+            this.navigatiePanel.Location = new System.Drawing.Point(16, 112);
+            this.navigatiePanel.Margin = new System.Windows.Forms.Padding(4);
+            this.navigatiePanel.Name = "navigatiePanel";
+            this.navigatiePanel.Size = new System.Drawing.Size(1035, 37);
+            this.navigatiePanel.TabIndex = 3;
+            // 
+            // openHelp
+            // 
+            this.openHelp.BackColor = System.Drawing.Color.White;
+            this.openHelp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.openHelp.Location = new System.Drawing.Point(921, 6);
+            this.openHelp.Margin = new System.Windows.Forms.Padding(4);
+            this.openHelp.Name = "openHelp";
+            this.openHelp.Size = new System.Drawing.Size(105, 27);
+            this.openHelp.TabIndex = 9;
+            this.openHelp.Text = "SLUIT HELP";
+            this.openHelp.UseVisualStyleBackColor = false;
+            this.openHelp.Click += new System.EventHandler(this.openHelp_Click_1);
+            // 
+            // faqButton
+            // 
+            this.faqButton.BackColor = System.Drawing.Color.White;
+            this.faqButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.faqButton.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.faqButton.Location = new System.Drawing.Point(517, 5);
+            this.faqButton.Margin = new System.Windows.Forms.Padding(4);
+            this.faqButton.Name = "faqButton";
+            this.faqButton.Size = new System.Drawing.Size(140, 28);
+            this.faqButton.TabIndex = 7;
+            this.faqButton.Text = "FAQ";
+            this.faqButton.UseVisualStyleBackColor = false;
+            this.faqButton.Click += new System.EventHandler(this.faqButton_Click);
+            // 
+            // contactButton
+            // 
+            this.contactButton.BackColor = System.Drawing.Color.White;
+            this.contactButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.contactButton.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactButton.Location = new System.Drawing.Point(352, 5);
+            this.contactButton.Margin = new System.Windows.Forms.Padding(4);
+            this.contactButton.Name = "contactButton";
+            this.contactButton.Size = new System.Drawing.Size(140, 28);
+            this.contactButton.TabIndex = 6;
+            this.contactButton.Text = "CONTACT";
+            this.contactButton.UseVisualStyleBackColor = false;
+            this.contactButton.Click += new System.EventHandler(this.contactButton_Click);
+            // 
+            // agendaButton
+            // 
+            this.agendaButton.BackColor = System.Drawing.Color.White;
+            this.agendaButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.agendaButton.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.agendaButton.Location = new System.Drawing.Point(187, 5);
+            this.agendaButton.Margin = new System.Windows.Forms.Padding(4);
+            this.agendaButton.Name = "agendaButton";
+            this.agendaButton.Size = new System.Drawing.Size(140, 28);
+            this.agendaButton.TabIndex = 5;
+            this.agendaButton.Text = "AGENDA";
+            this.agendaButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.agendaButton.UseVisualStyleBackColor = false;
+            this.agendaButton.Click += new System.EventHandler(this.agendaButton_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.BackColor = System.Drawing.Color.White;
+            this.homeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.homeButton.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeButton.Location = new System.Drawing.Point(23, 5);
+            this.homeButton.Margin = new System.Windows.Forms.Padding(4);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(140, 28);
+            this.homeButton.TabIndex = 4;
+            this.homeButton.Text = "HOME";
+            this.homeButton.UseVisualStyleBackColor = false;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // searchBar
+            // 
+            this.searchBar.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.searchBar.Location = new System.Drawing.Point(837, 80);
+            this.searchBar.Margin = new System.Windows.Forms.Padding(4);
+            this.searchBar.Name = "searchBar";
+            this.searchBar.Size = new System.Drawing.Size(212, 22);
+            this.searchBar.TabIndex = 4;
+            this.searchBar.Text = "Typ hier wat u zoekt...";
+            this.searchBar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // helpPanel1
+            // 
+            this.helpPanel1.Controls.Add(this.uitlegLabel1);
+            this.helpPanel1.Controls.Add(this.huidigePaginaLabel1);
+            this.helpPanel1.Controls.Add(this.hulpLabel1);
+            this.helpPanel1.Location = new System.Drawing.Point(772, 160);
+            this.helpPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.helpPanel1.Name = "helpPanel1";
+            this.helpPanel1.Size = new System.Drawing.Size(279, 379);
+            this.helpPanel1.TabIndex = 5;
+            this.helpPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.helpPanel1_Paint);
+            // 
+            // uitlegLabel1
+            // 
+            this.uitlegLabel1.AutoSize = true;
+            this.uitlegLabel1.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uitlegLabel1.Location = new System.Drawing.Point(-1, 105);
+            this.uitlegLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.uitlegLabel1.Name = "uitlegLabel1";
+            this.uitlegLabel1.Size = new System.Drawing.Size(250, 304);
+            this.uitlegLabel1.TabIndex = 3;
+            this.uitlegLabel1.Text = resources.GetString("uitlegLabel1.Text");
+            // 
+            // huidigePaginaLabel1
+            // 
+            this.huidigePaginaLabel1.AutoSize = true;
+            this.huidigePaginaLabel1.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.huidigePaginaLabel1.Location = new System.Drawing.Point(7, 64);
+            this.huidigePaginaLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.huidigePaginaLabel1.Name = "huidigePaginaLabel1";
+            this.huidigePaginaLabel1.Size = new System.Drawing.Size(233, 19);
+            this.huidigePaginaLabel1.TabIndex = 2;
+            this.huidigePaginaLabel1.Text = ". . . U bent nu op de HOME pagina.";
+            // 
+            // hulpLabel1
+            // 
+            this.hulpLabel1.AutoSize = true;
+            this.hulpLabel1.Font = new System.Drawing.Font("Malgun Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hulpLabel1.Location = new System.Drawing.Point(16, 23);
+            this.hulpLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.hulpLabel1.Name = "hulpLabel1";
+            this.hulpLabel1.Size = new System.Drawing.Size(143, 28);
+            this.hulpLabel1.TabIndex = 1;
+            this.hulpLabel1.Text = "Hulp nodig?...";
+            // 
+            // logoBox
+            // 
+            this.logoBox.BackColor = System.Drawing.Color.Transparent;
+            this.logoBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.logoBox.Image = ((System.Drawing.Image)(resources.GetObject("logoBox.Image")));
+            this.logoBox.Location = new System.Drawing.Point(335, 6);
+            this.logoBox.Margin = new System.Windows.Forms.Padding(4);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(392, 70);
+            this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoBox.TabIndex = 9;
+            this.logoBox.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(16, 160);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(745, 379);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(737, 350);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(737, 350);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(285, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HOME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(319, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AGENDA";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(737, 350);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(737, 350);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(319, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CONTACT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(319, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 32);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "FAQ";
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.navigatiePanel);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.searchBar);
+            this.Controls.Add(this.logoBox);
+            this.Controls.Add(this.helpPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "HomePage";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.homePage_Load);
+            this.navigatiePanel.ResumeLayout(false);
+            this.helpPanel1.ResumeLayout(false);
+            this.helpPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button contactButton;
+        private System.Windows.Forms.Button faqButton;
+        private System.Windows.Forms.Panel navigatiePanel;
+        private System.Windows.Forms.Button homeButton;
+        private System.Windows.Forms.Button agendaButton;
+        private System.Windows.Forms.TextBox searchBar;
+        private System.Windows.Forms.Panel helpPanel1;
+        private System.Windows.Forms.Label hulpLabel1;
+        private System.Windows.Forms.Label uitlegLabel1;
+        private System.Windows.Forms.Label huidigePaginaLabel1;
+        private System.Windows.Forms.PictureBox logoBox;
+        private System.Windows.Forms.Button openHelp;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EersteProjectMau/EersteProjectMau/HomePage.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EersteProjectMau
+{
+    public partial class HomePage : Form
+    {
+        private object tabControl;
+        Help_On_OFF helpKnop = new Help_On_OFF();
+
+        public void Help_Open_Sluit()
+        {
+            
+            helpPanel1.Visible = helpKnop.Screen;
+            helpKnop.Turn_ON_or_OFF();
+            openHelp.Text = (helpPanel1.Visible == false) ? "OPEN HELP" : "SLUIT HELP";
+            tabControl1.Size = (helpPanel1.Visible == false) ? new Size(770, 310) : new Size(550, 310);
+        }
+
+
+
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void homePage_Load(object sender, EventArgs e)
+        {
+            tabControl1.Appearance = TabAppearance.FlatButtons;
+            tabControl1.ItemSize = new Size(0, 1);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+
+        }
+
+
+        private void sluitHelp_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void openHelp_Click_1(object sender, EventArgs e)
+        {
+            Help_Open_Sluit();
+        }
+
+        private void faqButton_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(3);
+        }
+
+        private void noHelpFaqPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void faq_header_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            
+            tabControl1.SelectTab(0);
+        }
+
+        private void agendaButton_Click(object sender, EventArgs e)
+        {
+            
+            tabControl1.SelectTab(1);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contactButton_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(2);
+        }
+
+        private void helpPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
