8571744522fd6e4ee51030ddc702c75b44d9e8ec
ronanhub <45386974+ronanhub@users.noreply.github.com>
2020-05-31 18:29:55 +0200

    Selecteerbank melding en opslaan op juiste moment

---
+++ EersteProjectMau/EersteProjectMau/selecteerBankMelding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EersteProjectMau
+{
+    public partial class selecteerBankMelding : Form
+    {
+        public selecteerBankMelding()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSelecteerBankMeldingVerder_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
