9ffd3188efed69d802367615a96c3d298f569b99
mauricebunk <mauricebunk@users.noreply.github.com>
2020-06-01 20:53:48 +0200

    Revert "sprint62"
    This reverts commit 2ae313b3a06f239a01c4041328bf0d18323b7bd8.

---
+++ EersteProjectMau/EersteProjectMau/Verzonden.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EersteProjectMau
+{
+    
+    public partial class Verzonden : Form
+    {
+        public string closer = "open";
+        public Verzonden()
+        {
+            InitializeComponent();
+        }
+
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            closer = "Closing...";
+            this.Close();
+        }
+    }
+}
