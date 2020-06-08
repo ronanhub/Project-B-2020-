0c06d3d990bb90c2cbaa04b4b3108094a36dc4e3
mauricebunk <mauricebunk@users.noreply.github.com>
2020-06-08 23:08:37 +0200

    fimsbutton

--- EersteProjectMau/EersteProjectMau/Properties/Resources.Designer.cs
+++ EersteProjectMau/EersteProjectMau/Properties/Resources.Designer.cs
@@ -256,6 +256,16 @@ namespace EersteProjectMau.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap filmsButton1 {
+            get {
+                object obj = ResourceManager.GetObject("filmsButton1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap geenrobot {
             get {
                 object obj = ResourceManager.GetObject("geenrobot", resourceCulture);
